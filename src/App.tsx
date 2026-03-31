9dc1acf0ac70e18d5c0a116f3dc9e136a1bb84e0
Fahmi Rizal <fahmirizal91@gmail.com>
2026-03-31 16:30:42 +0700

    Update HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ChevronDown, Menu, X } from 'lucide-react'; // Tambahkan Menu dan X
 
 // Import semua halaman
